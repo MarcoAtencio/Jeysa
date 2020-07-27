92a75c32f28fe4dd3b586f265474b6e74bc81693
javieregc <n00083164@upn.pe>
2020-07-27 17:35:03 -0500

    clients2

--- resources/views/partials/dependenciesClient.blade.php
+++ resources/views/partials/dependenciesClient.blade.php
@@ -1,7 +1,7 @@
-<script src="Assets/Js/jquery.js" type="text/javascript"></script>
+<script src="{{asset('js/jquery.js')}}" type="text/javascript"></script>
 <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
-<script src="Assets/Js/pagination.js" type="text/javascript"></script>
+<script src="{{asset('js/pagination.js')}}" type="text/javascript"></script>
 <script src="https://kit.fontawesome.com/437756fd95.js" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
