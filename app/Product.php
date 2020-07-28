634e7754f494be761ea1feaccb9b10e41ddff969
MarcoAtencio <n00049623@upn.pe>
2020-07-28 10:54:25 -0500

    Imagees

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $appends = ["discount", "image1"];
+    protected $appends = ["discount", "image1","image2","image3"];
 }
